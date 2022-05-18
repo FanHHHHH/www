9aa091485470a4004b720db71eb77aabeb018fd8
hef <hefan813@vip.qq.com>
2022-05-18 17:42:41 +0800

    fix:git rm cache 误操作

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-import * as wasm from "hello-wasm-pack";
+import * as wasm from 'wasm-client'
 
-wasm.greet();
+// wasm.greet("faefaef");
+
+const myForm = document.getElementById('form')
+myForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+  const name = document.getElementById('name').value
+  const desc = document.getElementsByClassName('description')[0].value
+  wasm.add_course(name, desc).then((json) => {
+    alert('添加成功')
+    // window.location.reload()
+  })
+})
